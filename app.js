ab6995b45edb7e93496f42e95d1bb89af368e72d
amreshk005 <amreshk005@gmail.com>
2020-02-10 00:41:01 +0530

    handling cors

--- app.js
+++ app.js
@@ -13,6 +13,24 @@ const app = express();
 
 var cons = require("consolidate");
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  // res.header('Access-Control-Allow-Headers', '*');
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+
+  if (req.method === "OPTIONS") {
+    req.header(
+      "Access-Control-Allow-Methods",
+      "POST, POST, PATCH, DELETE, GET"
+    );
+    return res.status(200).json({});
+  }
+  next();
+});
+
 app.engine("html", cons.swig);
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "html");
